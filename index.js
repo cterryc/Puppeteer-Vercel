b19dcece65810e5421708357d888a17b035a9273
Terry <danyel.martel@gmail.com>
2024-08-24 13:07:43 -0500

    added scrap

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ if (production) {
   puppeteer = require("puppeteer");
 }
 
-app.get("/api", async (req, res) => {
+app.get("/api/:character", async (req, res) => {
+  const { character } = req.params;
+  const urlCharacter = `https://armory.warmane.com/character/${character}/Icecrown/summary`;
   try {
     const browser = await puppeteer.launch(
       production
@@ -27,9 +29,49 @@ app.get("/api", async (req, res) => {
         : {}
     );
 
-    let page = await browser.newPage();
-    await page.goto("https://www.google.com");
-    res.send(await page.title());
+    const page = await browser.newPage();
+    await page.goto(urlCharacter);
+    const elementos = await page.evaluate(() => {
+      const left = document.querySelectorAll(".item-left div div a");
+      const right = document.querySelectorAll(".item-right div div a");
+      const bottom = document.querySelectorAll(".item-bottom div div a");
+
+      // Función para extraer atributos de un nodo
+      const extractAttributes = (node) => {
+        const attrs = {};
+        for (const attr of node.attributes) {
+          attrs[attr.name] = attr.value;
+        }
+        return attrs;
+      };
+
+      // Extrae atributos de <a> y <img> (si existe) en los elementos
+      const extractElementsAttributes = (elements) => {
+        return Array.from(elements).map((ele) => {
+          const aAttributes = extractAttributes(ele);
+          const imgElement = ele.querySelector("img");
+          const imgAttributes = imgElement
+            ? extractAttributes(imgElement)
+            : null;
+
+          return { ...aAttributes, ...imgAttributes };
+        });
+      };
+
+      const leftAttributes = extractElementsAttributes(left);
+      const rightAttributes = extractElementsAttributes(right);
+      const bottomAttributes = extractElementsAttributes(bottom);
+
+      return {
+        left: leftAttributes,
+        right: rightAttributes,
+        bottom: bottomAttributes,
+      };
+    });
+
+    // Cierra el navegador
+    await browser.close();
+    res.status(200).send(elementos);
   } catch (err) {
     console.error(err);
     return null;
