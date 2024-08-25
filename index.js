121b134aef51b4410a577d1c5169117032ecae42
Terry <danyel.martel@gmail.com>
2024-08-25 05:28:16 -0500

    added new cors

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, ngrok-skip-browser-warning"
   );
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
   next();
